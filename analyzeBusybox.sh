39a64d4cb78ca3c984bc26a6cbffcd64594cbaf9
Christian Kaestner <christian.kaestner@uni-marburg.de>
2011-12-29 21:16:56 +0100

    fix paths

--- analyzeBusybox.sh
+++ analyzeBusybox.sh
@@ -9,11 +9,9 @@ filesToProcess() {
 
 
 flags="-U HAVE_LIBDMALLOC -DCONFIG_FIND -U CONFIG_FEATURE_WGET_LONG_OPTIONS -U ENABLE_NC_110_COMPAT -U CONFIG_EXTRA_COMPAT -D_GNU_SOURCE"
-srcPath="cprojects/busybox/busybox-1.18.5"
-export partialPreprocFlags="-x CONFIG_ --include busybox/config.h -I $srcPath/include"
-export outCSV=boa.csv
+srcPath="busybox-1.18.5"
+export partialPreprocFlags="-x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelFExpr busybox/featureModel --debugInterface --writePI --recordTiming"
 ## Reset output
-#echo -n > "$outCSV"
 filesToProcess|while read i; do
   if [ ! -f $srcPath/$i.dbg ]; then
     touch $srcPath/$i.dbg
