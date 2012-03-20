040da8f046dddbc0ce3012d1e09c4ec3530c99c5
Christian Kaestner <christian.kaestner@uni-marburg.de>
2012-03-20 14:07:28 +0100

    update scripts

---
+++ cleanBusyboxGit.sh
@@ -0,0 +1,1 @@
+find gitbusybox/ -name "*.dbg" |while read i; do echo $i; rm $i; done 
--- prepareGit.sh
+++ prepareGit.sh
@@ -9,13 +9,17 @@
 #you'll need KBuildMiner
 #http://code.google.com/p/variability/source/browse/KBuildMiner/
 #and Maven (mvn) to generate the list of presence conditions per files
+cd gitbusybox
+make gen_build_files
+cd ..
+
 cd KBuildMiner
 mvn scala:run -q -DmainClass=gsd.buildanalysis.linux.KBuildMinerMain
 cd ..
 
 grep -v libunarchive gitbusybox/pc.txt | grep -v Unknown > gitbusybox/pc_clean.txt
 
-cat KBuildMiner/pc.txt | sed s/\\.c:.*// | grep -v libunarchive > gitbusybox/filelist
+cat gitbusybox/pc.txt | sed s/\\.c:.*// | grep -v libunarchive > gitbusybox/filelist
 
 ./run.sh de.fosd.typechef.busybox.ProcessFileList gitbusybox/pc_clean.txt gitbusybox/
 
