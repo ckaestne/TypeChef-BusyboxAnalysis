92d2f4dce14fc0e9b3f75438649677c4c1db66c2
Christian Kästner <kaestner@informatik.uni-marburg.de>
2012-03-27 18:47:38 +0300

    describe possible dependency for mkrun

--- prepareBusybox.sh
+++ prepareBusybox.sh
@@ -4,6 +4,7 @@
 #* the header file
 #
 #run "sbt mkrun" to create run.sh the first time
+#(potentially you will need to compile and publish the main TypeChef project first with "sbt publish-local")
 #
 #create a directory  busybox-1.18.5 and download busybox sources there
 
