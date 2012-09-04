9dd04a8439eb6bae1de90762a353bccea8616aab
Christian Kaestner <christian.kaestner@uni-marburg.de>
2012-09-04 22:46:27 +0200

    remove more debug files

--- cleanBusybox.sh
+++ cleanBusybox.sh
@@ -1,1 +1,6 @@
-find busybox-1.18.5/ -name "*.dbg" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.dbg*" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.err" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.interface" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.pi" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.time" |while read i; do echo $i; rm $i; done 
+find busybox-1.18.5/ -name "*.c.xml" |while read i; do echo $i; rm $i; done 
